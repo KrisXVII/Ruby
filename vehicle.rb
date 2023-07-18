9b98fb624e3778b4f2a6bda536aad6524f466633
KrisXVII <kristianradulescu@gmail.com>
2023-07-18 14:41:12 +0200

    Staging stash module commit test

--- vehicle.rb
+++ vehicle.rb
@@ -3,11 +3,21 @@ class Vehicle
 
   def initialize(price, color)
     @price = price
-    self.color = color
+    @color = color.downcase
   end
 
   def to_s
     return "This #{color} vehicle is prized #{price} €"
+  end
+end
+
+module Usage
+  def usage
+    if self.color == 'red'
+      return "I am a sports car!"
+    else
+      return "Better go to job with this car"
+    end
   end
 end
 
@@ -17,6 +27,7 @@ module Go
   end
 end
 class Car < Vehicle
+  include Usage
   include Go
   attr_accessor :label, :type, :tires
 
@@ -32,6 +43,9 @@ class Car < Vehicle
   end
 end
 
+car1 = Car.new('car', 'BMW', '10.000', 'Red')
+print "car1: ", car1, "\n",
+      car1.usage
 car1 = Car.new('car', 'BMW', '10.000', 'red')
 print "car1: ", car1, "\n", car1.go
 
