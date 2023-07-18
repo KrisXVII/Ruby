31ed2dd1d1b812530f861c4feac52a8c060448e7
KrisXVII <kristianradulescu@gmail.com>
2023-07-18 18:09:44 +0200

    Done parsing not optimized

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,1 +1,13 @@
-puts "Project started"
+require 'csv'
+print 'Programma partito', "\n"
+
+file_to_parse = 'C:\Users\User\OneDrive\Documenti\Ruby\event_manager\lib\event_attendees.csv'
+
+lines = CSV.readlines(file_to_parse)
+header =  lines.shift
+lines.each do |line|
+  if line != header
+  name = line[2]
+  print name,"\n"
+  end
+end
