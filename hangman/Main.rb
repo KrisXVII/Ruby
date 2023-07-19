1177d8eb85ed7c52e22e3abba117a7aa093bb535
KrisXVII <kristianradulescu@gmail.com>
2023-07-19 18:23:57 +0200

    Remade main structure for UI guess

---
+++ hangman/Main.rb
@@ -0,0 +1,28 @@
+file = 'C:\Users\User\OneDrive\Documenti\Ruby\hangman\google-10000-english-no-swears.txt'
+
+class Word
+  def pick_random_line
+
+  end
+
+  def create_word_to_guess
+
+  end
+
+  attr_accessor :word_to_guess, :len
+
+  def underscores
+
+  end
+
+  def initialize
+    word_to_guess=create_word_to_guess
+    @len = word_to_guess.length
+  end
+end
+
+guesses = 0
+
+while guesses < 5
+
+end
