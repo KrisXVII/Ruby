7372edbe0e7938a0f4c5f8f887ced0f041fbfbe0
KrisXVII <kristianradulescu@gmail.com>
2023-07-20 10:58:51 +0200

    Written functions body
    Next to do is write the guessing process with char substitution and loss stage.

--- hangman/Main.rb
+++ hangman/Main.rb
@@ -1,28 +1,41 @@
 file = 'C:\Users\User\OneDrive\Documenti\Ruby\hangman\google-10000-english-no-swears.txt'
 
 class Word
-  def pick_random_line
-
+  def pick_random_line(file)
+    return File.readlines(file).sample
   end
 
-  def create_word_to_guess
-
+  def create_word_to_guess(file)
+    word_with_newline = pick_random_line(file).chars
+    word_with_newline.pop
+    word_without_newline = word_with_newline.join
+    n = word_without_newline.length
+    if n >= 5 and n<=12
+      return word_without_newline
+    else
+      create_word_to_guess(file)
+    end
   end
 
   attr_accessor :word_to_guess, :len
 
-  def underscores
-
+  def underscores(word)
+    underscore = String.new
+    return underscore.rjust(word.len, '_')
   end
 
-  def initialize
-    word_to_guess=create_word_to_guess
-    @len = word_to_guess.length
+  attr_accessor :word_to_guess, :len
+
+  def initialize(file)
+    @word_to_guess=create_word_to_guess(file)
+    @len = @word_to_guess.length
   end
 end
 
+word_x = Word.new(file)
+print  "Here's the word to guess: #{word_x.underscores(word_x)}  #{word_x.len} letters!"
 guesses = 0
 
-while guesses < 5
-
-end
+# while guesses < 5
+#
+# end
